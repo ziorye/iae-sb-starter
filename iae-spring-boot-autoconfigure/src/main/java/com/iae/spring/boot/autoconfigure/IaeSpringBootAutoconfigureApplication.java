3a7fdbe19191a6496406f49758a15d0cf7c6924d
ziorye <ziorye@gmail.com>
2023-10-27 17:22:26 +0800

    New ***-autoconfigure module using Spring Initializr

---
+++ iae-spring-boot-autoconfigure/src/main/java/com/iae/spring/boot/autoconfigure/IaeSpringBootAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.iae.spring.boot.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaeSpringBootAutoconfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaeSpringBootAutoconfigureApplication.class, args);
+	}
+
+}
