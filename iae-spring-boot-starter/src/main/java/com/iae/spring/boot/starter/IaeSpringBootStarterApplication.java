9b6bf9041d502d2f8422e7b61ec3fc517e2f999a
ziorye <ziorye@gmail.com>
2023-10-27 17:26:22 +0800

    New ***-starter module using Spring Initializr

---
+++ iae-spring-boot-starter/src/main/java/com/iae/spring/boot/starter/IaeSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.iae.spring.boot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaeSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaeSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ iae-spring-boot-starter/src/test/java/com/iae/spring/boot/starter/IaeSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iae.spring.boot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaeSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
